1bb07ffabafd0632c2bc1cbd38b961127a75a8bf
Brian Krabec <briankrabec@Brians-MacBook-Pro.local>
2020-04-30 12:05:03 -0500

    Created and set up models.py

---
+++ models.py
@@ -0,0 +1,25 @@
+from peewee import *
+import datetime
+
+from flask_login import UserMixin
+
+
+
+
+class User(UserMixin, Model):
+	username=CharField(unique=True)
+	email=CharField(unique=True)
+	password=CharField()
+
+	class Meta:
+		database = DATABASE
+
+
+
+
+def initialize():
+	DATABASE.connect()
+
+	DATABASE.create_tables([User], safe=True)
+	print("Connected to DB and created tables if they didn't already exist.")
+	DATABASE.close()
